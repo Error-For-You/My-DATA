73d1bc78325406d4d635181e78904591096136f2
Error 4 You <97140671+Error-For-You@users.noreply.github.com>
2022-01-05 22:02:21 +0530

    Create Data.py

---
+++ Data.py
@@ -0,0 +1,45 @@
+print("")
+print("")
+print("\033[1;31m _   _ ___         ")
+print("\033[1;31m| | | |_ _|        ")
+print("\033[1;31m| |_| || |         ")
+print("\033[1;31m|  _  || |         ")
+print("\033[1;31m|_| |_|___|        ")
+print("")
+print("")
+name=str(input('\033[1;32mENTE YOUR NAME : '))
+hello='               Hello '
+print("")
+print("\033[1;31m")
+print(hello+name)
+print("")
+age=input('\033[1;32mEnter your Age : ')
+print("")
+fm=input('\033[1;32mYou Are From : ')
+print("")
+scl=input('\033[1;32mYour School : ')
+print("")
+a='         Name          : '
+b='         Age           : '
+c='         I am from     : '
+d='         My school     : '
+
+print("\033[1;34m")
+print("__  ____   __  ____     ")
+print("|  \/  \ \ / / |  _ \   ")
+print("| |\/| |\ V /  | | | |  ")
+print("| |  | | | |   | |_| |  ")
+print("|_|  |_| |_|   |____/   ")
+print("\033[1;35m")
+print("")
+
+print(a+name)
+print("")
+print(b+age)
+print("")
+print(c+fm)
+print("")
+print(d+scl)
+
+print("")
+print("")
